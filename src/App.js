f2480c0a38e3d4f6c609679e1c3322b2a2e9547e
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2022-07-03 23:34:08 -0700

    Pulling extra data from contract to UI.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import './App.css';
 import React from "react";
+import web3 from "./web3"
 import lottery from "./lottery";
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {manager: ''};
+    this.state = {manager: '', players: [], balance: '', address: '', value: ''};
   }
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
-    this.setState({manager});
+    const address = lottery.options.address;
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address); // returns wey not ether.
+    this.setState({manager, players, balance, address});
   }
 
   render() {
-    return (
-        <div>
-          <h1>Lottery Contract</h1>
-          <p>This contract is <strong>managed by</strong>: {this.state.manager}</p>
-        </div>
-    );
+      const {manager, players, balance, address, value} = this.state;
+      const UNIT = "ether";
+      return (
+            <div>
+                 <h1>Lottery Contract</h1>
+                <p>This contract is <strong>managed by</strong>: {manager}</p>
+                <p>This contract is <strong> deployed to</strong>: {address}</p>
+
+                <div>
+                    <p>This contract currently contains {players.length} entries.</p>
+                    <p>The balance on the contract is: {web3.utils.fromWei(balance, UNIT)} ETH</p>
+                </div>
+
+
+                <div>
+                    <form>
+                        <h2>You should enter</h2>
+                        <label>Amount of ether to enter: </label>
+                        <input
+                            value = {value}
+                            onChange={(event) => {
+                                this.setState({value: event.target.value})
+                        }}/>
+                        <button>Enter</button>
+
+                    </form>
+                </div>
+            </div>
+
+        );
   }
 }
 
