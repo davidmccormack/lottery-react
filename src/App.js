98a4682fb878fc2a4ad04b3ea084d65822fbba31
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2022-07-05 13:51:53 -0700

    Added 'enter' feature to UI

--- src/App.js
+++ src/App.js
@@ -7,15 +7,38 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {manager: '', players: [], balance: '', address: '', value: ''};
+    this.state = {
+        manager: '',
+        players: [],
+        balance: '',
+        address: '',
+        value: '',
+        waiting: false,
+    };
   }
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
     const address = lottery.options.address;
     const players = await lottery.methods.getPlayers().call();
-    const balance = await web3.eth.getBalance(lottery.options.address); // returns wey not ether.
+    const balance = await web3.eth.getBalance(lottery.options.address); // returns wei not ether.
     this.setState({manager, players, balance, address});
+  }
+
+  onSubmit = async (event) => {
+      event.preventDefault();
+      const accounts = await web3.eth.getAccounts();
+      console.log(accounts)
+
+      this.setState({waiting: true})
+
+      const unit = "ether";
+      await lottery.methods.enter().send({
+          from: accounts[0],
+          value: web3.utils.toWei(this.state.value, unit),
+      })
+
+      this.setState({waiting: false})
   }
 
   render() {
@@ -27,24 +50,30 @@ class App extends React.Component {
                 <p>This contract is <strong>managed by</strong>: {manager}</p>
                 <p>This contract is <strong> deployed to</strong>: {address}</p>
 
+                <hr/>
+
                 <div>
                     <p>This contract currently contains {players.length} entries.</p>
                     <p>The balance on the contract is: {web3.utils.fromWei(balance, UNIT)} ETH</p>
                 </div>
 
+                <hr/>
 
                 <div>
                     <form>
-                        <h2>You should enter</h2>
+                        <h2>You should enter!</h2>
                         <label>Amount of ether to enter: </label>
                         <input
                             value = {value}
                             onChange={(event) => {
-                                this.setState({value: event.target.value})
+                                const unit = 'ether';
+                                this.setState({value: event.target.value,unit})
                         }}/>
-                        <button>Enter</button>
-
+                        <button onClick={this.onSubmit}>Enter</button>
                     </form>
+
+                    {this.state.waiting && <h1>Validating transaction...</h1>}
+
                 </div>
             </div>
 
--- src/web3.js
+++ src/web3.js
@@ -1,5 +1,7 @@
 import Web3 from "web3"
 
+window.ethereum.request({ method: "eth_requestAccounts" });
+
 // injecting provider from metamask's web3 version
 const web3 = new Web3(window.web3.currentProvider);
 
